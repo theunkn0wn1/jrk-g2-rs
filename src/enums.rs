520f909fa755e2103b4172755e7c29f00aa85374
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-12-08 05:15:42 +0100

    ugly PoC

--- src/enums.rs
+++ src/enums.rs
@@ -1,105 +1,108 @@
-enum VarOffset {
-    Input = 0x00,              // uint16_t
-    Target = 0x02,             // uint16_t
-    Feedback = 0x04,           // uint16_t
-    ScaledFeedback = 0x06,     // uint16_t
-    Integral = 0x08,           // int16_t
-    DutyCycleTarget = 0x0A,    // int16_t
-    DutyCycle = 0x0C,          // int16_t
-    CurrentLowRes = 0x0E,      // uint8_t
-    PIDPeriodExceeded = 0x0F,  // uint8_t
-    PIDPeriodCount = 0x10,     // uint16_t
-    ErrorFlagsHalting = 0x12,  // uint16_t
-    ErrorFlagsOccurred = 0x14, // uint16_t
+#[allow(dead_code)]
+pub enum VarOffset {
+    Input = 0x00,              // u16
+    Target = 0x02,             // u16
+    Feedback = 0x04,           // u16
+    ScaledFeedback = 0x06,     // u16
+    Integral = 0x08,           // i16
+    DutyCycleTarget = 0x0A,    // i16
+    DutyCycle = 0x0C,          // i16
+    CurrentLowRes = 0x0E,      // u8
+    PIDPeriodExceeded = 0x0F,  // u8
+    PIDPeriodCount = 0x10,     // u16
+    ErrorFlagsHalting = 0x12,  // u16
+    ErrorFlagsOccurred = 0x14, // u16
 
-    FlagByte1 = 0x16,  // uint8_t
-    VinVoltage = 0x17, // uint16_t
-    Current = 0x19,    // uint16_t
+    FlagByte1 = 0x16,  // u8
+    VinVoltage = 0x17, // u16
+    Current = 0x19,    // u16
 
     // variables above can be read with single-byte commands (GetVariable)
     // variables below must be read with segment read (GetVariables)
-    DeviceReset = 0x1F,                     // uint8_t
-    UpTime = 0x20,                          // uint32_t
-    RCPulseWidth = 0x24,                    // uint16_t
-    FBTReading = 0x26,                      // uint16_t
-    AnalogReadingSDA = 0x28,                // uint16_t
-    AnalogReadingFBA = 0x2A,                // uint16_t
-    DigitalReadings = 0x2C,                 // uint8_t
-    RawCurrent = 0x2D,                      // uint16_t
-    EncodedHardCurrentLimit = 0x2F,         // uint16_t
-    LastDutyCycle = 0x31,                   // int16_t
-    CurrentChoppingConsecutiveCount = 0x33, // uint8_t
-    CurrentChoppingOccurrenceCount = 0x34,  // uint8_t; read with dedicated command
+    DeviceReset = 0x1F,                     // u8
+    UpTime = 0x20,                          // u32
+    RCPulseWidth = 0x24,                    // u16
+    FBTReading = 0x26,                      // u16
+    AnalogReadingSDA = 0x28,                // u16
+    AnalogReadingFBA = 0x2A,                // u16
+    DigitalReadings = 0x2C,                 // u8
+    RawCurrent = 0x2D,                      // u16
+    EncodedHardCurrentLimit = 0x2F,         // u16
+    LastDutyCycle = 0x31,                   // i16
+    CurrentChoppingConsecutiveCount = 0x33, // u8
+    CurrentChoppingOccurrenceCount = 0x34,  // u8; read with dedicated command
 }
 
-enum SettingOffset {
-    OptionsByte1 = 0x01,                        // uint8_t
-    OptionsByte2 = 0x02,                        // uint8_t
-    InputMode = 0x03,                           // uint8_t
-    InputErrorMinimum = 0x04,                   // uint16_t,
-    InputErrorMaximum = 0x06,                   // uint16_t,
-    InputMinimum = 0x08,                        // uint16_t,
-    InputMaximum = 0x0A,                        // uint16_t,
-    InputNeutralMinimum = 0x0C,                 // uint16_t,
-    InputNeutralMaximum = 0x0E,                 // uint16_t,
-    OutputMinimum = 0x10,                       // uint16_t,
-    OutputNeutral = 0x12,                       // uint16_t,
-    OutputMaximum = 0x14,                       // uint16_t,
-    InputScalingDegree = 0x16,                  // uint8_t,
-    InputAnalogSamplesExponent = 0x17,          // uint8_t,
-    FeedbackMode = 0x18,                        // uint8_t,
-    FeedbackErrorMinimum = 0x19,                // uint16_t,
-    FeedbackErrorMaximum = 0x1B,                // uint16_t,
-    FeedbackMinimum = 0x1D,                     // uint16_t,
-    FeedbackMaximum = 0x1F,                     // uint16_t,
-    FeedbackDeadZone = 0x21,                    // uint8_t,
-    FeedbackAnalogSamplesExponent = 0x22,       // uint8_t,
-    SerialMode = 0x23,                          // uint8_t,
-    SerialBaudRateGenerator = 0x24,             // uint16_t,
-    SerialTimeout = 0x26,                       // uint16_t,
-    SerialDeviceNumber = 0x28,                  // uint16_t,
-    ErrorEnable = 0x2A,                         // uint16_t
-    ErrorLatch = 0x2C,                          // uint16_t
-    ErrorHard = 0x2E,                           // uint16_t
-    VinCalibration = 0x30,                      // uint16_t
-    PwmFrequency = 0x32,                        // uint8_t
-    CurrentSamplesExponent = 0x33,              // uint8_t
-    HardOvercurrentThreshold = 0x34,            // uint8_t
-    CurrentOffsetCalibration = 0x35,            // uint16_t
-    CurrentScaleCalibration = 0x37,             // uint16_t
-    FBTMethod = 0x39,                           // uint8_t
-    FBTOptions = 0x3A,                          // uint8_t
-    FBTTimingTimeout = 0x3B,                    // uint16_t
-    FBTSamples = 0x3D,                          // uint8_t
-    FBTDividerExponent = 0x3E,                  // uint8_t
-    IntegralDividerExponent = 0x3F,             // uint8_t
-    SoftCurrentRegulationLevelForward = 0x40,   // uint16_t
-    SoftCurrentRegulationLevelReverse = 0x42,   // uint16_t
-    OptionsByte3 = 0x50,                        // uint8_t
-    ProportionalMultiplier = 0x51,              // uint16_t
-    ProportionalExponent = 0x53,                // uint8_t
-    IntegralMultiplier = 0x54,                  // uint16_t
-    IntegralExponent = 0x56,                    // uint8_t
-    DerivativeMultiplier = 0x57,                // uint16_t
-    DerivativeExponent = 0x59,                  // uint8_t
-    PIDPeriod = 0x5A,                           // uint16_t
-    IntegralLimit = 0x5C,                       // uint16_t
-    MaxDutyCycleWhileFeedbackOutOfRange = 0x5E, // uint16_t
-    MaxAccelerationForward = 0x60,              // uint16_t
-    MaxAccelerationReverse = 0x62,              // uint16_t
-    MaxDecelerationForward = 0x64,              // uint16_t
-    MaxDecelerationReverse = 0x66,              // uint16_t
-    MaxDutyCycleForward = 0x68,                 // uint16_t
-    MaxDutyCycleReverse = 0x6A,                 // uint16_t
-    EncodedHardCurrentLimitForward = 0x6C,      // uint16_t
-    EncodedHardCurrentLimitReverse = 0x6E,      // uint16_t
-    BrakeDurationForward = 0x70,                // uint8_t
-    BrakeDurationReverse = 0x71,                // uint8_t
-    SoftCurrentLimitForward = 0x72,             // uint16_t
-    SoftCurrentLimitReverse = 0x74,             // uint16_t
+#[allow(dead_code)]
+pub enum SettingOffset {
+    OptionsByte1 = 0x01,                        // u8
+    OptionsByte2 = 0x02,                        // u8
+    InputMode = 0x03,                           // u8
+    InputErrorMinimum = 0x04,                   // u16,
+    InputErrorMaximum = 0x06,                   // u16,
+    InputMinimum = 0x08,                        // u16,
+    InputMaximum = 0x0A,                        // u16,
+    InputNeutralMinimum = 0x0C,                 // u16,
+    InputNeutralMaximum = 0x0E,                 // u16,
+    OutputMinimum = 0x10,                       // u16,
+    OutputNeutral = 0x12,                       // u16,
+    OutputMaximum = 0x14,                       // u16,
+    InputScalingDegree = 0x16,                  // u8,
+    InputAnalogSamplesExponent = 0x17,          // u8,
+    FeedbackMode = 0x18,                        // u8,
+    FeedbackErrorMinimum = 0x19,                // u16,
+    FeedbackErrorMaximum = 0x1B,                // u16,
+    FeedbackMinimum = 0x1D,                     // u16,
+    FeedbackMaximum = 0x1F,                     // u16,
+    FeedbackDeadZone = 0x21,                    // u8,
+    FeedbackAnalogSamplesExponent = 0x22,       // u8,
+    SerialMode = 0x23,                          // u8,
+    SerialBaudRateGenerator = 0x24,             // u16,
+    SerialTimeout = 0x26,                       // u16,
+    SerialDeviceNumber = 0x28,                  // u16,
+    ErrorEnable = 0x2A,                         // u16
+    ErrorLatch = 0x2C,                          // u16
+    ErrorHard = 0x2E,                           // u16
+    VinCalibration = 0x30,                      // u16
+    PwmFrequency = 0x32,                        // u8
+    CurrentSamplesExponent = 0x33,              // u8
+    HardOvercurrentThreshold = 0x34,            // u8
+    CurrentOffsetCalibration = 0x35,            // u16
+    CurrentScaleCalibration = 0x37,             // u16
+    FBTMethod = 0x39,                           // u8
+    FBTOptions = 0x3A,                          // u8
+    FBTTimingTimeout = 0x3B,                    // u16
+    FBTSamples = 0x3D,                          // u8
+    FBTDividerExponent = 0x3E,                  // u8
+    IntegralDividerExponent = 0x3F,             // u8
+    SoftCurrentRegulationLevelForward = 0x40,   // u16
+    SoftCurrentRegulationLevelReverse = 0x42,   // u16
+    OptionsByte3 = 0x50,                        // u8
+    ProportionalMultiplier = 0x51,              // u16
+    ProportionalExponent = 0x53,                // u8
+    IntegralMultiplier = 0x54,                  // u16
+    IntegralExponent = 0x56,                    // u8
+    DerivativeMultiplier = 0x57,                // u16
+    DerivativeExponent = 0x59,                  // u8
+    PIDPeriod = 0x5A,                           // u16
+    IntegralLimit = 0x5C,                       // u16
+    MaxDutyCycleWhileFeedbackOutOfRange = 0x5E, // u16
+    MaxAccelerationForward = 0x60,              // u16
+    MaxAccelerationReverse = 0x62,              // u16
+    MaxDecelerationForward = 0x64,              // u16
+    MaxDecelerationReverse = 0x66,              // u16
+    MaxDutyCycleForward = 0x68,                 // u16
+    MaxDutyCycleReverse = 0x6A,                 // u16
+    EncodedHardCurrentLimitForward = 0x6C,      // u16
+    EncodedHardCurrentLimitReverse = 0x6E,      // u16
+    BrakeDurationForward = 0x70,                // u8
+    BrakeDurationReverse = 0x71,                // u8
+    SoftCurrentLimitForward = 0x72,             // u16
+    SoftCurrentLimitReverse = 0x74,             // u16
 }
 
-enum JrkG2Error {
+#[allow(dead_code)]
+pub enum JrkG2Error {
     AwaitingCommand = 0,
     NoPower = 1,
     MotorDriver = 2,
@@ -119,7 +122,8 @@ enum JrkG2Error {
 /// This enum defines the Jrk G2 command bytes which are used for its serial and
 /// I2C interfaces.  These bytes are used by the library and you should not need
 /// to use them.
-enum JrkG2Command {
+#[allow(dead_code)]
+pub enum JrkG2Command {
     SetTarget = 0xC0,
     SetTargetLowResRev = 0xE0,
     SetTargetLowResFwd = 0xE1,
@@ -141,7 +145,8 @@ enum JrkG2Command {
 ///
 /// See JrkG2Base::getForceMode(), JrkG2Base::forceDutyCycleTarget(), and
 /// JrkG2Base::forceDutyCycle().
-enum JrkG2ForceMode {
+#[allow(dead_code)]
+pub enum JrkG2ForceMode {
     None = 0,
     DutyCycleTarget = 1,
     DutyCycle = 2,
@@ -151,7 +156,8 @@ enum JrkG2ForceMode {
 /// the Jrk G2.
 ///
 /// See JrkG2Base::getDeviceReset().
-enum JrkG2Reset {
+#[allow(dead_code)]
+pub enum JrkG2Reset {
     PowerUp = 0,
     Brownout = 1,
     ResetLine = 2,
@@ -162,7 +168,8 @@ enum JrkG2Reset {
 }
 
 /// This enum defines the Jrk G2's control and feedback pins.
-enum JrkG2Pin {
+#[allow(dead_code)]
+pub enum JrkG2Pin {
     SCL = 0,
     SDA = 1,
     TX = 2,
@@ -177,7 +184,8 @@ enum JrkG2Pin {
 /// should not need to use this directly.  See JrkG2Base::setResetIntegral(),
 /// JrkG2Base::getResetIntegral(), JrkG2Base::setCoastWhenOff(), and
 /// JrkG2Base::getCoastWhenOff().
-enum JrkG2OptionsByte3 {
+#[allow(dead_code)]
+pub enum JrkG2OptionsByte3 {
     ResetIntegral = 0,
     CoastWhenOff = 1,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,81 @@
+use core::fmt;
+use embedded_hal::{blocking::i2c, serial};
+use nb::block;
+
+mod enums;
+use enums::{JrkG2Command, VarOffset};
+
+pub struct JrkError;
+
+impl fmt::Debug for JrkError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "jrk error")
+    }
+}
+
+pub struct JrkBoard<I, S, E>
+where
+    I: i2c::Write<Error = nb::Error<E>> + i2c::Read<Error = nb::Error<E>>,
+    S: serial::Write<u8> + serial::Read<u8>,
+{
+    i2c: I,
+    ser: S,
+    device: u8,
+    use_i2c: bool,
+}
+
+impl<I, S, E> JrkBoard<I, S, E>
+where
+    I: i2c::Write<Error = nb::Error<E>> + i2c::Read<Error = nb::Error<E>>,
+    S: serial::Write<u8> + serial::Read<u8>,
+{
+    pub fn take(i2c: I, ser: S, device: u8, use_i2c: bool) -> JrkBoard<I, S, E> {
+        JrkBoard {
+            i2c,
+            ser,
+            device,
+            use_i2c,
+        }
+    }
+    pub fn write(&mut self, data: &[u8]) -> Result<(), JrkError> {
+        if self.use_i2c {
+            block!(self.i2c.write(self.device, &data)).map_err(|_e| JrkError {});
+        } else {
+            for &b in data.iter() {
+                block!(self.ser.write(b)).map_err(|_e| JrkError {});
+            }
+        }
+        Ok(())
+    }
+    pub fn read(&mut self, cmd: VarOffset) -> Result<u16, JrkError> {
+        self.write(&[JrkG2Command::GetVariable16 as u8 | (cmd as u8 + 1)])
+            .map_err(|_e| JrkError {});
+        if self.use_i2c {
+            let mut buf: [u8; 2] = [0, 0];
+            block!(self.i2c.read(self.device, &mut buf)).map_err(|_e| JrkError {})?;
+            Ok(buf[0] as u16 + buf[1] as u16 * 256)
+        } else {
+            let l = block!(self.ser.read()).map_err(|_e| JrkError {})?;
+            let h = block!(self.ser.read()).map_err(|_e| JrkError {})?;
+            Ok(l as u16 + h as u16 * 256)
+        }
+    }
+    pub fn set_target(&mut self, target: u16) -> Result<(), JrkError> {
+        self.write(&[
+            JrkG2Command::SetTarget as u8 + (target & 0x1F) as u8,
+            0x7F & (target >> 5) as u8,
+        ])
+    }
+    pub fn stop_motor(&mut self) -> Result<(), JrkError> {
+        self.write(&[JrkG2Command::MotorOff as u8])
+    }
+
+    pub fn switch(&mut self) -> bool {
+        self.use_i2c = !self.use_i2c;
+        self.use_i2c
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
